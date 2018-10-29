ad70d515b62839c1db471fc3493a7141fb8dbc3a
CrafMojo <setiawan.yanto@gmail.com>
2018-10-29 22:07:32 +0700

    add line again

--- Assignments/src/double_colon/Runner.java
+++ Assignments/src/double_colon/Runner.java
@@ -8,8 +8,30 @@ import java.util.function.Function;
  */
 
 public class Runner {
+	public void lambda(Lambda lambdas) {
+		lambdas.perform();
+	}
+
 	public static void main(String[] args) {
 		Function<Double, Double> square = x -> x * x * x;
 		System.out.println(square.apply(11d));
+		
+		// runnable
+		Thread abc = new Thread(() -> System.out.println("Printed Lambda"));
+		abc.run();
+
+		Lambda xyz = () -> System.out.println("Printed Lambda by Interface");
+		xyz.perform();
+		
+		Runner abcd = new Runner();
+		abcd.lambda(() -> System.out.println("Hello World!"));
+
+		mno uyy = () -> System.out.println("Printed Lambda by Interface Inline");
+		uyy.perform();
 	}
+}
+
+@FunctionalInterface
+interface mno {
+	public void perform();
 }
